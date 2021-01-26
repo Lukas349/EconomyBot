eb0170f156d667f2598a692f533a31cbd51327a7
Lukas349 <77998859+Lukas349@users.noreply.github.com>
2021-01-26 07:24:47 -0400

    Update
    misuuu

--- botConfig.js
+++ botConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "Nzk2MDM1OTczMTQ2MDgzNDM4.X_SERQ.sPhxhMcWKEBtcm_81pQ1rGf1W8E",
+    prefix: "k?",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "787125350978814022"
 ],
     debug: true,
-    countChannel: "countChannelID"
+    countChannel: "799969849329057792"
 };
